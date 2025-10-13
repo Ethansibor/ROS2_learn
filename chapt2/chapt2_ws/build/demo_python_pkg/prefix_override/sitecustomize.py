5b0a143aa6bc8f2a16a888ca8790f59d380f8b28
Ethansibor <2276250774@qq.com>
2025-10-13 11:34:13 +0800

    Untrack build/install/log and enforce .gitignore

--- chapt2/chapt2_ws/build/demo_python_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ethan/ros2/chapt2/chapt2_ws/install/demo_python_pkg'
--- chapt2/chapt2_ws/install/demo_python_pkg/lib/python3.10/site-packages/demo_python_pkg/python_node.py
+++
@@ -1,9 +0,0 @@
-import rclpy
-from rclpy.node import Node
-
-def main():
-    rclpy.init()
-    node = Node("python_node")
-    node.get_logger().info('hello python node')
-    rclpy.spin(node)
-    rclpy.shutdown()
