5a4aa4d16eb70fc2c2cea79dfd5790e5631da601
Ethansibor <2276250774@qq.com>
2025-10-13 10:23:21 +0800

    first_commit

---
+++ chapt2/chapt2_ws/src/demo_python_pkg/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'demo_python_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ethan',
+    maintainer_email='ethan@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'python_node = demo_python_pkg.python_node:main',
+            'person_node = demo_python_pkg.person_node:main',
+            'writer_node = demo_python_pkg.writer_node:main',
+            'learn_thread = demo_python_pkg.learn_thread:main',
+            'hello_world_class_node = demo_python_pkg.hello_world_class_node:main',
+            'object_node = demo_python_pkg.object_node:main',
+        ],
+    },
+)
